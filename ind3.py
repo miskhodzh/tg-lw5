b74f75c3805affb3356c02ce43d0337ab11ce3a3
miskhodzh <miskhodzhev.osman@yandex.ru>
2022-10-18 23:14:43 +0300

    Исправил ошибку в задании 3

--- ind3.py
+++ ind3.py
@@ -1,7 +1,9 @@
 word = input('Введите слово >>> ')
 n = int(input('Введите номер символа >>>'))
 
-word = word.replace(word[2], '')
-word = word.replace(word[n], '')
+if n > 3:
+    word = word[:2]+word[3:n]+word[n+1:]
+elif n < 3:
+    word = word[:n]+word[n+1:2]+word[3:]
 
 print(word)
